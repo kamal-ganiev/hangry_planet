b8c53d3f3f98ba418856af366cad63b0b4061eeb
kamal-ganiev <kamal.ganiev999@gmail.com>
2024-02-26 14:25:42 -0500

    add interactivity to menu nav

---
+++ scripts/constantArrayLists.js
@@ -0,0 +1,77 @@
+// Menu Lists
+
+const burgersList = [
+  {
+    name: "The Big Bang",
+    price: "$ 13",
+    ingredients: "Pulled Beef | Lettuce | Tomato | Onion | Pickle | Cheese",
+  },
+  {
+    name: "Galaxy Beef-burger",
+    price: "$ 11",
+    ingredients: "Beef patty | Lettuce | Tomato | Onion | Pickle | Cheese",
+  },
+  {
+    name: "Cosmo Chicken",
+    price: "$ 10",
+    ingredients: "Chicken patty | Lettuce | Tomato | Pickle | Cheese",
+  },
+  {
+    name: "Solar Fish",
+    price: "$ 10",
+    ingredients: "Fried Fish | Cole Slaw | Pickle | Tartar sauce",
+  },
+  {
+    name: "Eggs-plorer",
+    price: "$ 11",
+    ingredients: "Scrambled Egg | Bacon | Lettuce | Cheese",
+  },
+  {
+    name: "Pluto-bean",
+    price: "$ 10",
+    ingredients: "Bean patty | Lettuce | Tomato | Onion | Pickle | Cheese",
+  },
+];
+
+const sidesList = [
+  {
+    name: "French Fries",
+    price: "$ 7",
+    ingredients: "Made with Potatoes from K-245L, aka Idaho!",
+  },
+  {
+    name: "Loaded fries",
+    price: "$ 11",
+    ingredients: "Add bacon and cheese to your fries!",
+  },
+  {
+    name: "Chicken Nuggets",
+    price: "$ 10",
+    ingredients: "Does it fly? Does it shine?",
+  },
+  {
+    name: "Beverages",
+    price: "$ 2",
+    ingredients: "There is no fish in the cans, I promise...",
+  },
+];
+
+const breakfastList = [
+  {
+    name: "Eggs-plorer",
+    price: "$ 11",
+    ingredients: "Scrambled egg | Bacon | Lettuce | Cheese",
+  },
+  {
+    name: "Salmon Way",
+    price: "$ 12",
+    ingredients: "Smoked Salmon | Tomato | Arugula | Cream cheese",
+  },
+  {
+    name: "Coffee",
+    price: "$ 2",
+    ingredients: "Energy that can turn back on the Sun",
+  },
+];
+
+export { burgersList, sidesList, breakfastList };
--- scripts/script.js
+++ scripts/script.js
@@ -5,15 +5,19 @@ const headerTitle = document.querySelector(".header__title");
 
 function shrinkHeader() {
   if (document.body.scrollTop > 72 || document.documentElement.scrollTop > 72) {
+    headerTitle.classList.add("header__title_disappear");
     header.classList.add("header_shrinked");
     headerBackground.classList.add("header__background_shrinked");
     logo.classList.add("header__logo_shrinked");
-    headerTitle.classList.add("header__title_disappear");
+    headerTitle.classList.add("header__title_disable");
   } else {
     header.classList.remove("header_shrinked");
     headerBackground.classList.remove("header__background_shrinked");
     logo.classList.remove("header__logo_shrinked");
     headerTitle.classList.remove("header__title_disappear");
+    setTimeout(() => {
+      headerTitle.classList.remove("header__title_disable");
+    }, 300);
   }
 }
 
