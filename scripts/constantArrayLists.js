faa45afed25b621d0d6c48472c8efbb8c7fa725e
kamal-ganiev <kamal.ganiev999@gmail.com>
2024-02-26 22:05:55 -0500

    add interactivity to calendar

--- scripts/constantArrayLists.js
+++ scripts/constantArrayLists.js
@@ -5,31 +5,37 @@ const burgersList = [
     name: "The Big Bang",
     price: "$ 13",
     ingredients: "Pulled Beef | Lettuce | Tomato | Onion | Pickle | Cheese",
+    isVeg: false,
   },
   {
     name: "Galaxy Beef-burger",
     price: "$ 11",
     ingredients: "Beef patty | Lettuce | Tomato | Onion | Pickle | Cheese",
+    isVeg: false,
   },
   {
     name: "Cosmo Chicken",
     price: "$ 10",
     ingredients: "Chicken patty | Lettuce | Tomato | Pickle | Cheese",
+    isVeg: false,
   },
   {
     name: "Solar Fish",
     price: "$ 10",
     ingredients: "Fried Fish | Cole Slaw | Pickle | Tartar sauce",
+    isVeg: false,
   },
   {
     name: "Eggs-plorer",
     price: "$ 11",
     ingredients: "Scrambled Egg | Bacon | Lettuce | Cheese",
+    isVeg: false,
   },
   {
     name: "Pluto-bean",
     price: "$ 10",
     ingredients: "Bean patty | Lettuce | Tomato | Onion | Pickle | Cheese",
+    isVeg: true,
   },
 ];
 
@@ -38,21 +44,25 @@ const sidesList = [
     name: "French Fries",
     price: "$ 7",
     ingredients: "Made with Potatoes from K-245L, aka Idaho!",
+    isVeg: true,
   },
   {
     name: "Loaded fries",
     price: "$ 11",
     ingredients: "Add bacon and cheese to your fries!",
+    isVeg: false,
   },
   {
     name: "Chicken Nuggets",
     price: "$ 10",
     ingredients: "Does it fly? Does it shine?",
+    isVeg: false,
   },
   {
     name: "Beverages",
     price: "$ 2",
     ingredients: "There is no fish in the cans, I promise...",
+    isVeg: false,
   },
 ];
 
@@ -61,17 +71,83 @@ const breakfastList = [
     name: "Eggs-plorer",
     price: "$ 11",
     ingredients: "Scrambled egg | Bacon | Lettuce | Cheese",
+    isVeg: false,
   },
   {
     name: "Salmon Way",
     price: "$ 12",
     ingredients: "Smoked Salmon | Tomato | Arugula | Cream cheese",
+    isVeg: false,
   },
   {
     name: "Coffee",
     price: "$ 2",
     ingredients: "Energy that can turn back on the Sun",
+    isVeg: false,
   },
 ];
 
-export { burgersList, sidesList, breakfastList };
+const listOfEvents = [
+  {
+    id: 1,
+    month: "Jan",
+    data: [],
+  },
+  {
+    id: 2,
+    month: "Feb",
+    data: [],
+  },
+  {
+    id: 3,
+    month: "Mar",
+    data: [],
+  },
+  {
+    id: 4,
+    month: "Apr",
+    data: [],
+  },
+  {
+    id: 5,
+    month: "May",
+    data: [],
+  },
+  {
+    id: 6,
+    month: "Jun",
+    data: [],
+  },
+  {
+    id: 7,
+    month: "Jul",
+    data: [],
+  },
+  {
+    id: 8,
+    month: "Aug",
+    data: [],
+  },
+  {
+    id: 9,
+    month: "Sep",
+    data: [],
+  },
+  {
+    id: 10,
+    month: "Oct",
+    data: [],
+  },
+  {
+    id: 11,
+    month: "Nov",
+    data: [],
+  },
+  {
+    id: 12,
+    month: "Dec",
+    data: [],
+  },
+];
+
+export { burgersList, sidesList, breakfastList, listOfEvents };
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,21 @@
+import {
+  burgersList,
+  breakfastList,
+  sidesList,
+  listOfEvents,
+} from "./constantArrayLists.js";
+
+const date = new Date();
+
 const header = document.querySelector(".header");
 const headerBackground = document.querySelector(".header__background");
 const logo = document.querySelector(".header__logo-contaner");
 const headerTitle = document.querySelector(".header__title");
+
+const menuContainer = document.querySelector(".menu__list");
+const menuTemplate = document.querySelector(".menu__template").content;
+const menuNavbar = document.querySelector(".menu__nav");
+const menuNavItems = document.querySelectorAll(".menu__nav-item");
 
 function shrinkHeader() {
   if (document.body.scrollTop > 72 || document.documentElement.scrollTop > 72) {
@@ -24,3 +38,117 @@ function shrinkHeader() {
 window.onscroll = () => {
   shrinkHeader();
 };
+
+function switchMenu(list) {
+  list.forEach((item) => {
+    const newCard = menuTemplate.cloneNode(true);
+
+    const cardName = newCard.querySelector(".menu__item-name");
+    const cardPrice = newCard.querySelector(".menu__item-price");
+    const cardDescription = newCard.querySelector(".menu__item-description");
+    const cardIsVeg = newCard.querySelector(".menu__veggie-sign");
+
+    cardName.textContent = item.name;
+    cardPrice.textContent = item.price;
+    cardDescription.textContent = item.ingredients;
+    if (item.isVeg) {
+      cardIsVeg.style = "display: block";
+    } else {
+      cardIsVeg.style = "display: none";
+    }
+
+    menuContainer.appendChild(newCard);
+  });
+}
+
+switchMenu(burgersList);
+
+function toggleSwitch(list, target) {
+  list.forEach((e) => {
+    e.children[0].classList.remove("menu__nav-checkbox-checked");
+    e.children[0].children[0].classList.remove(
+      "menu__nav-checkbox-inner_visible"
+    );
+  });
+
+  console.log(target);
+
+  target.children[0].classList.add("menu__nav-checkbox-checked");
+  target.children[0].children[0].classList.add(
+    "menu__nav-checkbox-inner_visible"
+  );
+}
+
+menuNavbar.children[0].addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.target;
+  menuContainer.innerHTML = "";
+  switchMenu(burgersList);
+
+  toggleSwitch(menuNavItems, e.target);
+});
+
+menuNavbar.children[1].addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.target;
+  menuContainer.innerHTML = "";
+  switchMenu(breakfastList);
+
+  toggleSwitch(menuNavItems, e.target);
+});
+
+menuNavbar.children[2].addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.target;
+  menuContainer.innerHTML = "";
+  switchMenu(sidesList);
+
+  toggleSwitch(menuNavItems, e.target);
+});
+
+const month = document.querySelector(".calendar__month");
+const monthName = month.querySelector(".calendar__month-name");
+let monthNum = date.getMonth();
+let currentMonth = listOfEvents[monthNum].month;
+
+const eventBack = month.querySelector(".calendar__month-switch-b");
+const eventForward = month.querySelector(".calendar__month-switch-f");
+
+monthName.textContent = currentMonth;
+
+if (monthNum === 0) {
+  eventBack.style = "display: none";
+  monthNum = 0;
+} else if (monthNum === 12) {
+  eventForward.style = "display: none";
+  monthNum = 12;
+} else {
+  eventBack.style = "display: inline";
+  eventForward.style = "display: inline";
+}
+
+eventBack.addEventListener("click", () => {
+  monthNum -= 1;
+
+  monthName.textContent = listOfEvents[monthNum].month;
+
+  if (monthNum === 0) {
+    eventBack.style = "display: none";
+  } else {
+    eventBack.style = "display: inline";
+    eventForward.style = "display: inline";
+  }
+});
+
+eventForward.addEventListener("click", () => {
+  monthNum += 1;
+
+  monthName.textContent = listOfEvents[monthNum].month;
+
+  if (monthNum === 11) {
+    eventForward.style = "display: none";
+  } else {
+    eventBack.style = "display: inline";
+    eventForward.style = "display: inline";
+  }
+});
